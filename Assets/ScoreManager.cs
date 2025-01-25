f7eeda49f9d8a9a2e95f437d463959f01bcbc33c
KyleWoo2619 <wkhgate2619@gmail.com>
2025-01-25 12:27:55 -0500

    Enemy AI created

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; // use TMP
+
+public class ScoreManager : MonoBehaviour
+{
+    public int playerScore = 0; // player score
+    public int enemyScore = 0; // enemy score
+
+    [Header("Player ")]
+    public TMP_Text playerScoreTMP; // put player score here
+    public TMP_Text enemyScoreTMP; // put enemy score here
+
+    // increase player score
+    public void AddPlayerScore()
+    {
+        playerScore++;
+        UpdateScoreUI();
+    }
+
+    // increase enemy score
+    public void AddEnemyScore()
+    {
+        enemyScore++;
+        UpdateScoreUI();
+    }
+
+    // update score
+    void UpdateScoreUI()
+    {
+        if (playerScoreTMP != null)
+        {
+            playerScoreTMP.text = "Player Score: " + playerScore;
+        }
+
+        if (enemyScoreTMP != null)
+        {
+            enemyScoreTMP.text = "Enemy Score: " + enemyScore;
+        }
+    }
+}
