f701a3990ad4371643310029c9677602593005c9
KyleWoo2619 <wkhgate2619@gmail.com>
2025-01-29 18:25:47 -0500

    Update EnemyAI_Revised.cs

--- Assets/EnemyAI_Revised.cs
+++ Assets/EnemyAI_Revised.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class EnemyAI_Revised : MonoBehaviour
 {
 
     public Transform player; //getting the player location
+    public Transform bulletSpawn;//where cannon is shot from
+    public GameObject bulletPrefab;//bullet
     public float movespeed; //enemy AI speed
-    public bool canMove = true;
+    public float shootingRate; //enemy shooting rate
+    private float rotationSpeed;
+    public bool canMove = true; //turret mode or tank mode
+
+    private float currentSpeed = 0f;
+    private float acceleration = 2f;
+    private float deceleration = 5f;
+    private float nextShootTime = 0f;
+    private bool isRotating = false;
 
     void Update()
     {
-
-        Movement();
         Shooting();
+        Rotation();
+        Movement();
 
     }
     void Movement()
@@ -26,12 +37,49 @@ public class EnemyAI_Revised : MonoBehaviour
             //consistent movement to the player
             direction.Normalize();
 
+            if (currentSpeed < movespeed)
+            {
+                currentSpeed += acceleration * Time.deltaTime;
+                currentSpeed = Mathf.Clamp(currentSpeed, 0, movespeed);
+            }
+
             transform.position = Vector3.MoveTowards(transform.position, player.position, movespeed);
+
+            isRotating = false;
+        }
+
+        else
+        {
+            currentSpeed = Mathf.Lerp(currentSpeed, 0f, deceleration * Time.deltaTime);
+
+            isRotating = true;
+        }
+    }
+
+    void Rotation()
+    {
+        if (isRotating && !canMove)
+        {
+            Vector3 direction = player.position - transform.position;
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
     }
 
     void Shooting()
     {
-        
+
+        if (Time.time >= nextShootTime)
+        {
+            ShootBullet();
+            nextShootTime = Time.time; + shootingRate;
+        }
+    }
+
+    void ShootBullet()
+    {
+
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation); //create the bullet prefab in the scene
+        bullet.GetComponent<Bullet>().Initialize();
     }
 }
